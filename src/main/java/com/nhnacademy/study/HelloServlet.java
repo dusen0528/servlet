15b9080c8f4c90eea72d87392121195a33e6b8f4
inho <dusen0528@naver.com>
2025-01-16 16:43:10 +0900

    chap08

---
+++ src/main/java/com/nhnacademy/study/HelloServlet.java
@@ -0,0 +1,64 @@
+package com.nhnacademy.study;
+
+import java.io.*;
+import java.util.Objects;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebServlet(name = "helloServlet", value = "/index.html")
+public class HelloServlet  extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+        String title = getServletConfig().getInitParameter("title");
+        String name = getServletConfig().getInitParameter("name");
+
+        if(Objects.isNull(title)){
+            title = "Mr.";
+        }
+        if(Objects.isNull(name)){
+            name = "marco";
+        }
+
+        resp.setCharacterEncoding("utf-8");
+        try(PrintWriter writer = resp.getWriter()) {
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<meta charset='utf-8'>");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println("<h1>hello servlet!</h1>");
+            writer.println("<h1>안녕 서블릿!</h1>");
+            writer.printf("<h1>hello %s %s</h1>\n", title, name);
+            writer.println("</body>");
+            writer.println("</html>");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        log.info("before init!");
+        super.init(config);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("before service!");
+        super.service(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        log.info("before destroy!");
+        super.destroy();
+    }
+}
